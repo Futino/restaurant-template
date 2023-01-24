e17eb9b7d63f445c475c24f116157e29cae938ce
Jorge Lewis <antopiahk@gmail.com>
2023-01-24 16:15:36 +0800

    home page fixup

--- src/components/pages/home/mod.rs
+++ src/components/pages/home/mod.rs
@@ -17,79 +17,26 @@ impl Component for Home {
             <main>
 
             // Top section
-            <div class="top-0 py-60 px-4 sm:px-6 md:px-8 border-b dark:border-secondary-dark/40 shadow-2xl bg-[url('/images/curry.jpg')] bg-cover object-cover bg-center">
-            <div class="relative max-w-6xl mx-auto">
+            <div class="top-0 py-60 px-4 sm:px-6 md:px-8 border-b dark:border-secondary-dark/40 shadow-2xl bg-[url('/images/burger.jpg')] bg-cover object-contain  bg-center">
+            <div class="relative max-w-6xl mx-auto space-y-4">
                 <Label>
-                    <h1 class="font-extrabold text-8xl sm:text-5xl lg:text-6xl m-5">
-                        {"Allheim WMA/HEMA"}
+                    <h1 class="font-normal font-bilbo text-8xl text-primary-light">
+                        {"Welcome"}
                     </h1>
                 </Label>
 
                 <Label>
-                    <h2 class="text-4xl">
-                        {"Come with famiy and friends to feel the joy of hitting them with swords"}
+                    <h2 class="text-8xl tracking-wider font-ubuntu text-primary-on-light border-y border-primary-on-light max-w-fit mx-auto">
+                        {"SOUTHSIDE LANTAU"}
                     </h2>
                 </Label>
-
-                <div class="mt-6 sm:mt-10 flex justify-center space-x-6 text-sm">
-                    <a href="/contact">
-                        <Button btn_type="button">
-                            <p>{"Contact Us!"}</p>
-                        </Button>
-                    </a>
-                    <a href="/about">
-                        <Button btn_type="button">
-                            <p>{"Who Are We?"}</p>
-                        </Button>
-                    </a>
-                </div>
+                <Label>
+                    <h2 class="text-2xl text-primary-on-light ">
+                        {"AMERICAN SOUL FOOD"}
+                    </h2>
+                </Label>
             </div>
         </div>
-
-            // Various Menus Options
-            <div class="justify-center py-20 mx-40 bg-black">
-                <div class="p-8 pb-12 mx-auto max-w-md text-center">
-                        <h4 class="p-4 text-sm font-light text-primary-200">
-                            {
-                                "FLAVOURS FOR ROYALTY"
-                            }
-                        </h4>
-                        <SeperatorIcon />
-                        <h1 class="p-6 text-5xl font-normal text-white">
-                            {"We Offer Top Notch"}
-                        </h1>
-                        <p class="text-sm font-light leading-6 text-white">
-                            {
-                                "Lorem Ipsum is simply dummy text of the printing and typesetting
-                                industry lorem Ipsum has been the industrys standard dummy text ever."
-                            }
-                        </p>
-
-                    </div>
-                    <div class="grid grid-cols-3 gap-x-8 p-6 text-center">
-                        <div class="p-6 pt-4">
-                            <img src="/images/pancakes.jpeg" class="object-cover h-full w-fit" alt="" />
-                            <h1 class="p-6 text-3xl text-white">
-                                {"Breakfast"}
-                            </h1>
-                            <TextLink text="VIEW MENU" href="/Menu"/>
-                        </div>
-                        <div class="p-6">
-                            <img src="/images/pancakes.jpeg" class="object-cover h-full w-fit" alt="" />
-                            <h1 class="p-6 text-3xl text-white">
-                                {"Desserts"}
-                            </h1>
-                            <TextLink text="VIEW MENU" href="/Menu"/>
-                        </div>
-                        <div class="p-6 pt-4">
-                            <img src="/images/pancakes.jpeg" class="object-cover h-full w-fit" alt="" />
-                            <h1 class="p-6 text-3xl text-white">
-                                {"Drinks"}
-                            </h1>
-                            <TextLink text="VIEW MENU" href="/Menu"/>
-                        </div>
-                    </div>
-            </div>
 
             // About section
             <div class="bg-stone-900">
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,90 +10,90 @@ module.exports = {
       colors: {
         // Primary tones
         primary: {
-          light: "rgb(var(--md-color-primary-light)/ <alpha-value>)",
-          dark: "rgb(var(--md-color-primary-dark) / <alpha-value>)",
+          light: "rgb(var(--md-sys-color-primary-light)/ <alpha-value>)",
+          dark: "rgb(var(--md-sys-color-primary-dark) / <alpha-value>)",
           on: {
-            light: "rgb(var(--md-color-on-primary-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-on-primary-dark) / <alpha-value>) ",
+            light: "rgb(var(--md-sys-color-on-primary-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-on-primary-dark) / <alpha-value>) ",
           },
           container: {
             light:
-              "rgb(var(--md-color-primary-container-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-primary-container-dark) / <alpha-value>) ",
+              "rgb(var(--md-sys-color-primary-container-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-primary-container-dark) / <alpha-value>) ",
             on: {
               light:
-                "rgb(var(--md-color-on-primary-container-light)/ <alpha-value>) ",
-              dark: "rgb(var(--md-color-on-primary-container-dark) / <alpha-value>) ",
+                "rgb(var(--md-sys-color-on-primary-container-light)/ <alpha-value>) ",
+              dark: "rgb(var(--md-sys-color-on-primary-container-dark) / <alpha-value>) ",
             },
           },
           inverse:
           {
             light:
-              "rgb(var(--md-color-inverse-primary-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-on-primary-container-dark) / <alpha-value>) ",
+              "rgb(var(--md-sys-color-inverse-primary-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-on-primary-container-dark) / <alpha-value>) ",
           }
         },
 
         // Secondary tones
         secondary: {
-          light: "rgb(var(--md-color-secondary-light)/ <alpha-value>) ",
-          dark: "rgb(var(--md-color-secondary-dark) / <alpha-value>) ",
+          light: "rgb(var(--md-sys-color-secondary-light)/ <alpha-value>) ",
+          dark: "rgb(var(--md-sys-color-secondary-dark) / <alpha-value>) ",
           on: {
-            light: "rgb(var(--md-color-on-secondary-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-on-secondary-dark) / <alpha-value>) ",
+            light: "rgb(var(--md-sys-color-on-secondary-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-on-secondary-dark) / <alpha-value>) ",
           },
           container: {
             light:
-              "rgb(var(--md-color-secondary-container-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-secondary-container-dark) / <alpha-value>) ",
+              "rgb(var(--md-sys-color-secondary-container-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-secondary-container-dark) / <alpha-value>) ",
             on: {
               light:
-                "rgb(var(--md-color-on-secondary-container-light)/ <alpha-value>) ",
-              dark: "rgb(var(--md-color-on-secondary-container-dark) / <alpha-value>) ",
+                "rgb(var(--md-sys-color-on-secondary-container-light)/ <alpha-value>) ",
+              dark: "rgb(var(--md-sys-color-on-secondary-container-dark) / <alpha-value>) ",
             },
           },
         },
 
         // Tertiary tones
         tertiary: {
-          light: "rgb(var(--md-color-tertiary-light)/ <alpha-value>) ",
-          dark: "rgb(var(--md-color-tertiary-dark) / <alpha-value>) ",
+          light: "rgb(var(--md-sys-color-tertiary-light)/ <alpha-value>) ",
+          dark: "rgb(var(--md-sys-color-tertiary-dark) / <alpha-value>) ",
           on: {
-            light: "rgb(var(--md-color-on-tertiary-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-on-tertiary-dark) / <alpha-value>) ",
+            light: "rgb(var(--md-sys-color-on-tertiary-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-on-tertiary-dark) / <alpha-value>) ",
           },
           container: {
             light:
-              "rgb(var(--md-color-tertiary-container-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-tertiary-container-dark) / <alpha-value>) ",
+              "rgb(var(--md-sys-color-tertiary-container-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-tertiary-container-dark) / <alpha-value>) ",
             on: {
               light:
-                "rgb(var(--md-color-on-tertiary-container-light)/ <alpha-value>) ",
-              dark: "rgb(var(--md-color-on-tertiary-container-dark) / <alpha-value>) ",
+                "rgb(var(--md-sys-color-on-tertiary-container-light)/ <alpha-value>) ",
+              dark: "rgb(var(--md-sys-color-on-tertiary-container-dark) / <alpha-value>) ",
             },
           },
         },
 
         // Neutral tones (md3 names them as 'surface')
         surface: {
-          light: "rgb(var(--md-color-surface-light)/ <alpha-value>) ",
-          dark: "rgb(var(--md-color-surface-dark) / <alpha-value>) ",
+          light: "rgb(var(--md-sys-color-surface-light)/ <alpha-value>) ",
+          dark: "rgb(var(--md-sys-color-surface-dark) / <alpha-value>) ",
           on: {
             inverse: {
-              light: "rgb(var(--md-color-inverse-on-surface-light)/ <alpha-value>) ",
-              dark: "rgb(var(--md-color-inverse-on-surface-dark) / <alpha-value>) ",
+              light: "rgb(var(--md-sys-color-inverse-on-surface-light)/ <alpha-value>) ",
+              dark: "rgb(var(--md-sys-color-inverse-on-surface-dark) / <alpha-value>) ",
             },
-            light: "rgb(var(--md-color-on-surface-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-on-surface-dark) / <alpha-value>) ",
+            light: "rgb(var(--md-sys-color-on-surface-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-on-surface-dark) / <alpha-value>) ",
           },
           // Neutral variant tones
           variant:
           {
-            light: "rgb(var(--md-color-neutral-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-neutral-dark) / <alpha-value>) ",
+            light: "rgb(var(--md-sys-color-neutral-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-neutral-dark) / <alpha-value>) ",
             on: {
-              light: "rgb(var(--md-color-on-neutral-light)/ <alpha-value>) ",
-              dark: "rgb(var(--md-color-on-neutral-dark) / <alpha-value>) ",
+              light: "rgb(var(--md-sys-color-on-neutral-light)/ <alpha-value>) ",
+              dark: "rgb(var(--md-sys-color-on-neutral-dark) / <alpha-value>) ",
             },
           }
         },
@@ -101,52 +101,52 @@ module.exports = {
         // Background tones
         background: {
           light:
-            "rgb(var(--md-color-background-light)/ <alpha-value>) ",
-          dark: "rgb(var(--md-color-background-dark) / <alpha-value>) ",
+            "rgb(var(--md-sys-color-background-light)/ <alpha-value>) ",
+          dark: "rgb(var(--md-sys-color-background-dark) / <alpha-value>) ",
           on: {
             light:
-              "rgb(var(--md-color-on-background-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-on-background-dark) / <alpha-value>) ",
+              "rgb(var(--md-sys-color-on-background-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-on-background-dark) / <alpha-value>) ",
           },
         },
 
         outline:
         {
           light:
-            "rgb(var(--md-color-outline-light)/ <alpha-value>) ",
-          dark: "rgb(var(--md-color-outline-dark) / <alpha-value>) ",
+            "rgb(var(--md-sys-color-outline-light)/ <alpha-value>) ",
+          dark: "rgb(var(--md-sys-color-outline-dark) / <alpha-value>) ",
           variant: {
             light:
-              "rgb(var(--md-color-outline-variant-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-outline-variant-dark) / <alpha-value>) ",
+              "rgb(var(--md-sys-color-outline-variant-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-outline-variant-dark) / <alpha-value>) ",
           }
         },
 
         // On Error tones
         error: {
           light:
-            "rgb(var(--md-color-error-light)/ <alpha-value>) ",
-          dark: "rgb(var(--md-color-error-dark) / <alpha-value>) ",
+            "rgb(var(--md-sys-color-error-light)/ <alpha-value>) ",
+          dark: "rgb(var(--md-sys-color-error-dark) / <alpha-value>) ",
           on: {
             light:
-              "rgb(var(--md-color-on-error-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-on-error-dark) / <alpha-value>) ",
+              "rgb(var(--md-sys-color-on-error-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-on-error-dark) / <alpha-value>) ",
           },
           container: {
             light:
-              "rgb(var(--md-color-error-container-light)/ <alpha-value>) ",
-            dark: "rgb(var(--md-color-error-container-light)/ <alpha-value>) ",
+              "rgb(var(--md-sys-color-error-container-light)/ <alpha-value>) ",
+            dark: "rgb(var(--md-sys-color-error-container-light)/ <alpha-value>) ",
             on: {
               light:
-                "rgb(var(--md-color-on-error-container-light)/ <alpha-value>) ",
-              dark: "rgb(var(--md-color-on-error-container-dark) / <alpha-value>) ",
+                "rgb(var(--md-sys-color-on-error-container-light)/ <alpha-value>) ",
+              dark: "rgb(var(--md-sys-color-on-error-container-dark) / <alpha-value>) ",
             },
           },
         },
       },
       fontFamily: {
-        sans: ["Graphik", "sans-serif"],
-        serif: ["Merriweather", "serif"],
+        bilbo: ["Bilbo Swash Caps"],
+        ubuntu: ["Ubuntu"]
       },
       borderWidth: {
         1: "1px",
